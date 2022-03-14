49d296a962c77c352ab541d1cf0065da83eaa185
Mehedi Hasan Nirob <75689692+Nirob-0812@users.noreply.github.com>
2022-03-14 23:37:15 +0600

    Lab-4

---
+++ CSE-304/Lab/NewtonDevideDifferenceInterpolation.cpp
@@ -0,0 +1,51 @@
+/*
+Mehedi Hasan Nirob
+Roll: 04
+Batch: Day-59
+Department of CSE
+Dhaka International University
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define nl endl
+#define sp fixed<<setprecision(5) 
+#define Nirob int main()
+#define getfor(i,n) for(int i=0;i<n;i++) 
+Nirob
+{
+    int n;
+    cout<<"How much value do you want to insert: ";
+    cin>>n;
+    double x[n],y[n],dif[n][n],X,a,h,p,yp,dp=1,fact=1;
+    cout<<"Give values of Xi and Yi:"<<nl;
+    getfor(i,n)
+    cin>>x[i]>>y[i];
+    a=x[0];
+    h=x[1]-x[0];
+    cout<<"Enter value of X: ";
+    cin>>X;
+    for(int i=0;i<n-1;i++)
+    {
+        dif[i][1]=(y[i+1]-y[i])/(x[i+1]-x[i]);
+        cout<<sp<<"D1Y"<<i<<": "<<dif[i][1]<<"\t";
+    }
+    cout<<nl;
+    for(int j=2;j<n;j++)
+    {
+        for(int i=0;i<n-j;i++)
+        {
+            dif[i][j]=(dif[i+1][j-1]-dif[i][j-1])/(x[i+j]-x[i]);
+            cout<<sp<<"D"<<j<<"Y"<<i<<": "<<dif[i][j]<<"\t";
+        }
+        cout<<nl;
+    }
+    yp=y[0];
+    for(int i=1;i<n;i++)
+    {
+        dp=dp*(X-x[i-1]);
+        yp=yp+dp*dif[0][i];
+    }
+    cout<<sp<<"For X="<<X<<", The Interpolated value of Y is: "<<yp<<nl;
+
+  return 0;
+}
